423f3a95b90c9ca81b57e25bec8d61629bd8011f
emen7 <44346972+emen7@users.noreply.github.com>
2025-03-13 23:52:33 -0400

    Add styled-components and TypeScript types, and create CMS home page

---
+++ apps/cms/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CMSHomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the CMS</h1>
+      <p>This is the CMS home page.</p>
+    </div>
+  );
+};
+
+export default CMSHomePage;
